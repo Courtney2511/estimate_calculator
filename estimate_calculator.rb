d69aee6fd6b5be957e125d49c4a3841a05d08b80
Courtney Noonan <courtneynoonan@me.com>
2017-03-14 14:14:57 -0400

    getting example tests to pass

--- estimate_calculator.rb
+++ estimate_calculator.rb
@@ -1,15 +1,30 @@
 
 # markups base price 5%
 def flat_markup(base)
-  (base * 0.05).round(2)
+  (base * 1.05)
 end
 
 # markups base price 1.2% per worker on the job
 def worker_markup(base, workers)
-  (base * workers * 0.012).round(2)
+  flat_markup(base) * (workers * 0.012)
 end
 
-# base plus first markups
-def cost_before_material_markup(base, workers)
-  base + flat_markup(base) + worker_markup(base, workers)
+def category_markup(category)
+  category_markup = ''
+    if category == 'drugs'
+      category_markup = 0.075
+    elsif category == 'food'
+      category_markup =  0.13
+    elsif category == 'electronics'
+      category_markup =  0.02
+    else
+      category_markup = 0
+    end
+  category_markup
 end
+
+def estimate_calculator(base, workers, category)
+  (flat_markup(base) + worker_markup(base, workers) + (flat_markup(base) * category_markup(category))).round(2)
+end
+
+puts category_markup('food')
